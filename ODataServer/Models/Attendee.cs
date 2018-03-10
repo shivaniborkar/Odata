7376af1609abb867ffee64610ef4d73b58f7299d
shivani borkar <shivani.borkar@yahoo.com>
2018-03-10 11:50:22 +0100

    removing extra properties

--- ODataServer/Models/Attendee.cs
+++ ODataServer/Models/Attendee.cs
@@ -12,9 +12,7 @@ namespace ODataServer.Models
         public string name { get; set; }
         public int age { get; set; }
 
-        public int dummyProperty { get; set; }
-        public int dummyProperty1 { get; set; }
-        public int dummyProperty2 { get; set; }
+       
 
     }
 }
